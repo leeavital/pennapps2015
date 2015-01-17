2559fe4403103c2b0ee6ef3c37e2f367e7a70a0f
Lee Avital <leeavital@gmail.com>
2015-01-17 06:11:59 -0500

    Move stuff

--- NLP/parser.py
+++ NLP/parser.py
@@ -85,6 +85,12 @@ def get_latest():
     return jsonify(f)
 
 
+@app.route('/', methods=['GET'])
+def static_index():
+    return render_template("index.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
