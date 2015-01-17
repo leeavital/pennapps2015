621f0053e1aa0484986c96fad3ec92cfcea4a607
Lee Avital <leeavital@gmail.com>
2015-01-17 14:38:14 -0500

    Add preposition backfilling

--- NLP/parser.py
+++ NLP/parser.py
@@ -34,7 +34,9 @@ def get_entities():
         corefs = result['coref'][0]
     for sent in result['sentences']:
         # pp.pprint(sent['dependencies'])
+        print corefs
         for dep in sent['dependencies']:
+            print dep
             if dep[0] == 'det':
                 entities[dep[1]] = []
             elif dep[0] == 'amod' or dep[0] == 'nsubj':
@@ -43,32 +45,39 @@ def get_entities():
                         entities[dep[1]].append(dep[2])
                     elif dep[2] in entities:
                         entities[dep[2]].append(dep[1])
-
             elif 'prep_' in dep[0]:
-                # entities[dep[1]] = dep[2]
-                if dep[1] in entities and dep[2] in entities:
-                    deps.append([dep[1], dep[0], dep[2]])
-                else:
-                    for coref in corefs:
-                        obj1 = coref[0]
-                        obj2 = coref[1]
-                        # print obj1[0], obj2[0], dep[1]
-                        if dep[1] in obj1[0]:
-                            print (dep[2], dep[0], obj1[0])
-                            # deps.append([dep[2], dep[0], obj[1]])
-                        elif dep[1] in obj2[0]:
-                            print (dep[2], dep[0], obj2[0])
-                            # deps.append([obj1[0], dep[0], obj[2]])
-                        elif dep[2] in obj1[0]:
-                            name = [z for z in obj2[0].split() if z in entities][0]
-                            deps.append([dep[1], dep[0], name])
-                            # deps.append([obj2[0], dep[0], dep[1]])
-                        elif dep[2] in obj2[0]:
-                            name = [z for z in obj2[0].split() if z in entities][0]
+              if dep[1] == 'is': # backfill with nsubj
+                try:
+                  print "backfilling..."
+                  print deps
+                  dep[1] = [x for x in sent["dependencies"] if x[0] == 'nsubj' and x[1] == 'is'][0][2]
+                  print "backfilled", dep
+                except:
+                  pass
+              # entities[dep[1]] = dep[2]
+              if dep[1] in entities and dep[2] in entities:
+                  deps.append([dep[1], dep[0], dep[2]])
+              else:
+                  for coref in corefs:
+                      obj1 = coref[0]
+                      obj2 = coref[1]
+                      # print obj1[0], obj2[0], dep[1]
+                      if dep[1] in obj1[0]:
+                          print (dep[2], dep[0], obj1[0])
+                          # deps.append([dep[2], dep[0], obj[1]])
+                      elif dep[1] in obj2[0]:
+                          print (dep[2], dep[0], obj2[0])
+                          # deps.append([obj1[0], dep[0], obj[2]])
+                      elif dep[2] in obj1[0]:
+                          name = [z for z in obj2[0].split() if z in entities][0]
+                          deps.append([dep[1], dep[0], name])
+                          # deps.append([obj2[0], dep[0], dep[1]])
+                      elif dep[2] in obj2[0]:
+                          name = [z for z in obj2[0].split() if z in entities][0]
 
-                            deps.append([dep[1], dep[0], name])
+                          deps.append([dep[1], dep[0], name])
 
-                            # deps.append([obj1[0], dep[0], dep[1]])
+                          # deps.append([obj1[0], dep[0], dep[1]])
 
     # deps = set(deps)
     f = {}
@@ -126,9 +135,6 @@ class ModelDownloader(threading.Thread):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=80)
-
-    ModelDownloader(["banana"]).run()
-
+    app.run(host='0.0.0.0', port=80)
 
 
